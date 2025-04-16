0c63102ec9a4d94dc18a88c436b124acf9581a61
Technology for protection secure data <dmitryak150308@gmail.com>
2025-04-16 23:15:12 +0300

    pre-final view of dowload

--- dowload.sh
+++ dowload.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 apt install -y git
 git clone https://github.com/Cipheator/ciphator_for_security/.
+
+
 systemctl enable --now systemd-networkd
-mv 10-static.network /etc/systemd/network/
 systemctl restart systemd-networkd
-useradd client
+
+useradd -m -s /bin/bash client
+echo 'client:1' | chpasswd
+usermod -aG sudo client
+
+systemctl start ssh
+xhost +SI:localuser:client
+
 su client
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 ssh-copy-id worker@192.168.1.2
